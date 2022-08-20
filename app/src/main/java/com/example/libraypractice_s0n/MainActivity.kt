617501d4d2fe9c418d0ceeb357c9ca022c34798b
s0n <shson971029@naver.com>
2022-08-20 23:03:02 +0900

    권한 라이브러리 활용 예시 화면 작성

--- app/src/main/java/com/example/libraypractice_s0n/MainActivity.kt
+++ app/src/main/java/com/example/libraypractice_s0n/MainActivity.kt
@@ -20,9 +20,16 @@ class MainActivity : AppCompatActivity() {
         }
 
         //인터넷에 있는 이미지 (https:// ...)을 이미지뷰에 바로 대입.
+//        참고)매니패스트의 <application태그에서 usesCleartextTraffic을 true로 설정해주면 http://...도 가능.
         Glide.with(this).load("https://postfiles.pstatic.net/MjAyMjA4MDhfMjgy/MDAxNjU5OTAxNzE0NzM5.WkF3DuEiaEl_n7WbCgNkq8Jq7_NnrWkPk2mRQw742owg.2hKGR-iLZ7FONiy6dNIQckTrnPNimARBWkb52Yb" +
                 "rAhIg.JPEG.bk_s0n/IMG_0592.JPG?type=w580").into(myImg1)
 
+        //전화 걸기 버튼 누르면 -> 권한 확인 하고, 전화 연결
+
+        callBtn.setOnClickListener {
+
+            //라이브러리 활용 -> 전화 권환 확인 -> 실제 전화 연결
+        }
 
     }
 }
